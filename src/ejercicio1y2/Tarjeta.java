30a0c9da66372f1c5dbdeab397cddc8810c9005e
xrosilloprofe <xv.rosillo@edu.gva.es>
2023-12-05 20:25:42 +0100

    v0.2

--- src/ejercicio1y2/Tarjeta.java
+++ src/ejercicio1y2/Tarjeta.java
@@ -9,4 +9,48 @@ public class Tarjeta {
     private double saldo;
     private double limite;
 
+    public Tarjeta(Cuenta cuentaAsociada, double limite){
+        this.cuentaAsociada=cuentaAsociada;
+        this.titular= cuentaAsociada.getTitular();
+        this.numTarjeta=TIPO_TARJETA+num;
+        num++;
+        this.limite=limite;
+        this.saldo=0;
+    }
+
+    public Tarjeta(Cuenta cuentaAsociada, double limite, String titular){
+        this(cuentaAsociada,limite);
+        this.titular=titular;
+    }
+
+    public Cuenta getCuentaAsociada(){
+        return cuentaAsociada;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public String getNumTarjeta() {
+        return numTarjeta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setLimite(double limite){
+        this.limite=limite;
+    }
+
+
+    @Override
+    public String toString(){
+        return numTarjeta + " de: " + titular + " limite: " + limite + " saldo: " + saldo + " \n cuenta asociada: " + cuentaAsociada.toString();
+    }
+
 }
